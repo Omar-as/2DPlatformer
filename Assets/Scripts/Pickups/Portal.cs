d3240952c33f9e8078099e38f1a8382b55ed7dd9
Omar Al Asaad <omarelassaad93@gmail.com>
2022-04-05 18:00:38 +0300

    Added a portal script and finalized level 1

---
+++ Assets/Scripts/Pickups/Portal.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private Transform destination;
+
+    public Boolean isRed;
+
+    public float distance = 0.2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isRed == false)
+        {
+            destination = GameObject.FindGameObjectWithTag("Red Portal").GetComponent<Transform>();
+        }
+        else
+        {
+            destination = GameObject.FindGameObjectWithTag("Yellow Portal").GetComponent<Transform>();
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            if (Vector3.Distance(transform.position, other.transform.position) > distance)
+            {
+                GameObject.Find("Player").transform.position = new Vector3(destination.position.x, destination.position.y);
+            }
+        }
+    }
+}
